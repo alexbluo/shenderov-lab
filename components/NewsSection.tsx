8bc4fdd383c974179b402392793c023daac8cff2
Alex Luo <alexluo92823@gmail.com>
2025-01-03 18:28:59 -0500

    fix news date and researchimage dimensions

--- components/NewsSection.tsx
+++ components/NewsSection.tsx
@@ -32,16 +32,20 @@ const NewsSection = () => {
           voluptates, cumque rerum qui velit perspiciatis saepe possimus? Velit,
           necessitatibus.
         </NewsBlock> */}
-        <NewsBlock headline="Promising Immunotherapy to Treat Localized High-Risk Prostate Cancer" date="February , 2024">
-        For men with localized high-risk prostate cancer the HEAT trial is active across multiple locations 
-        in the United States and investigating if a promising immunotherapy can increase the cure rate of 
-        surgery and prevent cancer recurrence.
-        <br/>
-        <a
+        <NewsBlock
+          headline="Promising Immunotherapy to Treat Localized High-Risk Prostate Cancer"
+          date="February, 2024"
+        >
+          For men with localized high-risk prostate cancer the HEAT trial is
+          active across multiple locations in the United States and
+          investigating if a promising immunotherapy can increase the cure rate
+          of surgery and prevent cancer recurrence.
+          <br />
+          <a
             href="https://clinicaltrials.gov/study/NCT06014255"
             target="_blank"
             rel="noopener noreferrer"
-            style={{ color: 'white', textDecoration: 'underline' }}
+            style={{ color: "white", textDecoration: "underline" }}
           >
             https://clinicaltrials.gov/study/NCT06014255
           </a>
